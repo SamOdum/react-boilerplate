0c09bda75932be8cb21d2f08aff91f07f31cc398
Sam Odum <sam.odum1@gmail.com>
2019-05-28 13:51:59 +0100

    Set up hot reloading and a few other utilities

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 
 class App extends Component {
-    render() { 
-        return ( <h1>Hello World</h1> );
-    }
+	state = {
+		count: 0,
+	};
+
+	handleClickPlus = () => {
+		this.setState({ count: this.state.count + 1 });
+	};
+
+	handleClickMinus = () => {
+		const currentCount =
+			this.state.count !== 0 ? this.state.count - 1 : (this.state.count = 0);
+		this.setState({
+			count: currentCount,
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<h1>Hello World</h1>
+				<h2>Count: {this.state.count}</h2>
+				<button onClick={this.handleClickPlus}>+</button>
+				<button onClick={this.handleClickMinus}>-</button>
+			</div>
+		);
+	}
 }
- 
-export default App;
+
+export default hot(module)(App);
